0a2074d252c47f15c899aa7fd499cd5429398e5a
koolshiva <koolshiva@gmail.com>
2016-09-23 17:36:17 +0530

    modified server js to serve template page html content.

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var pageData = {'title':'Shiva Web Page','heading':'Welcome to Express JS Web App','content':'You are viewing the contents served from a express Web Server.'};
+
+function createTemplate(data){
+	var title = data.title, heading = data.heading, content = data.content;console.log(data.title);
+	var htmlTemplate = '<html><head><title>'+data.title+'</title><link href="/ui/style.css" rel="stylesheet"/></head><body><h1>'+data.heading+'</h1><p>'+data.content+'</p></body></html>';
+	return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  //res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.send(createTemplate(pageData));
 });
 
 app.get('/ui/style.css', function (req, res) {
